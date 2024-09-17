616b97cef906fa12766ef817a5a97f854d5de082
Akkshay Tandon <76265879+akkshayTandon@users.noreply.github.com>
2024-09-17 23:38:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 // const portfolio = document.getElementById('link-portfolio');
-const portfolio = document.querySelector('.links');
-// console.log(portfolio)
-portfolio.addEventListener("mouseover", () => {
-    // portfolio.innerHTML = `Under Construction 👷‍♂️`;
-    portfolio.innerHTML = `<a id="link-portfolio" target="_blank" class="link"> Under Construction 👷‍♂️ </a>`
-});
+// const portfolio = document.querySelector('.links');
+// // console.log(portfolio)
+// portfolio.addEventListener("mouseover", () => {
+//     // portfolio.innerHTML = `Under Construction 👷‍♂️`;
+//     portfolio.innerHTML = `<a id="link-portfolio" target="_blank" class="link"> Under Construction 👷‍♂️ </a>`
+// });
 
-portfolio.addEventListener("mouseout", (e) => {
-    // console.log(e);
-    portfolio.innerHTML = `<a id="link-portfolio" target="_blank" class="link"> Portfolio <i class="fa fa-solid fa-user fa-lg"></i> </a>`;
-});
+// portfolio.addEventListener("mouseout", (e) => {
+//     // console.log(e);
+//     portfolio.innerHTML = `<a id="link-portfolio" target="_blank" class="link"> Portfolio <i class="fa fa-solid fa-user fa-lg"></i> </a>`;
+// });
 
 /* Sidebar Opening and Closing */
 const sidebar = document.getElementById("sidebar");
 const menuBtn = document.getElementById("menu-btn");
 const cancelBtn = document.getElementById("cancel-btn");
-const projectBtn = document.querySelectorAll('.links')[1];
+const projectBtn = document.querySelectorAll('.links')[0];
 
 const header = document.getElementById("header");
 const linkContainer = document.getElementById("link-container");
